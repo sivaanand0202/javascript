39d201bf0092fc9fa611f609c15cea65ff110a8b
Nivetha <nivetha@10decoders.in>
2019-04-16 12:08:04 +0530

    initial code added

---
+++ index.js
@@ -0,0 +1,8 @@
+var fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+var citrus = fruits.slice(1,3);
+console.log(":Fruits:", fruits);
+console.log(":Citrus:", citrus);
+fruits.splice(1,3)
+console.log(":----> 2222222. > >C c v.:", fruits);
+ // document.getElementById("demo").innerHTML = fruits + "<br><br>" + citrus;
+
